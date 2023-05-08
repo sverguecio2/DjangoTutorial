9f462c0c58dd91d0c77ea8256b7dbb55b08a8e33
sverguecio3 <132927982+sverguecio3@users.noreply.github.com>
2023-05-08 20:30:46 -0300

    actualizo las vistas
    Nueva update

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 
 class CreateNewTask(forms.Form):
-    title = forms.CharField(max_length=200, label="Task Title")
+    title = forms.CharField(max_length=200, label="Task Title", widget=forms.TextInput(attrs={'class' : 'form-control'}))
     description = forms.CharField(widget=forms.Textarea, label="Task Description", required=False)
     
 class CreateNewProject(forms.Form):
-    name = forms.CharField(max_length=200, label="Project Name")
+    name = forms.CharField(max_length=200, label="Nombre del Proyecto", widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    description = forms.CharField(label="Descripcion del Proyecto", required=False, widget=forms.Textarea(attrs={'class' : 'form-control'}))
+    
+    #done = forms.BooleanField(label="Terminado", required=False, widget=forms.CheckboxInput(attrs={'class' : 'form-check-input'}))
+
+    
+
+    
--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,9 @@ from django.db import models
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
+    description = models.TextField( blank=True)
+    done = models.BooleanField(default=False)
+    
 
     def __str__(self):
         return self.name
